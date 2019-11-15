9607d988975f54155ce510b148013e358e410282
zhenghy16 <1241984077@qq.com>
2019-11-15 21:21:37 +0800

    Please enter the commit message for your changes. Lines starting aawith '#' will be ignored, and an empty message aborts the commit.

--- lab3local.py
+++ lab3local.py
@@ -1,4 +1,9 @@
 from scapy.all import *
-a = TCP_SERVICE
-a.send(HTTPRequest())
-a.recv()
+port=18478
+f=open('content.txt','rb')
+s=f.read()
+IP(dst='188.166.199.77')/TCP(sport=port,flags='S',dport=80)
+a.show()    
+ans=sr(a)
+b=IP(dst='188.166.199.77')/TCP(sport=port,ack=ans[0][0][1][1].seq+1,seq=1,flags='A',dport=80)
+ans2=sr(b)
